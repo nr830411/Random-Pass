53c9b86339a406e69e5ce8e567d43c5c52a09cde
Nico Reed <nr830411@gmail.com>
2019-11-20 11:50:36 -0500

    (created pass variables and random generator function. some css added to page/content)

--- dajava.js
+++ dajava.js
@@ -1,8 +1,17 @@
 
+//generate random pass
 
+function generate(){
 
+    var random = document.getElementById("slider").value;
 
+    var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
+    var password = "";
 
+    for(var i = 0; i <= random; i++){
 
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
 
+    }
+}
